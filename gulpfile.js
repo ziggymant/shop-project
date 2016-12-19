4187c75dcce0a05fd0551cff6c72fa93123f1b69
Zygimantas Makunas <z.makunas@gmail.com>
2016-12-19 13:33:39 +0000

    added admin layout

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ require('laravel-elixir-vue-2');
 
 elixir(mix => {
     mix.sass('app.scss')
-       .webpack('app.js')
+       // .webpack('app.js')
 
 
        .styles([
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index');
 
 Route::resource('/admin/users', 'AdminUsersController');
+
+Route::get('/admin', function(){
+
+	return view('admin.index');
+});
