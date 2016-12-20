0f676ec69c2385266e260b0202dedae5a072f3c8
Zygimantas Makunas <z.makunas@gmail.com>
2016-12-20 11:09:09 +0000

    Added all inputs to the user:create form

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Role;
 
 class AdminUsersController extends Controller
 {
@@ -25,7 +26,8 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        return view('admin.users.create');
+        $roles = Role::pluck('name','id')->all();
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -36,7 +38,7 @@ class AdminUsersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return $request->all();
     }
 
     /**
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -6,4 +6,35 @@
 
 
 
+{!!Form::open(['method'=>'POSTS', 'action'=>'AdminUsersController@store'])!!}
+
+	<div class="form-group">
+		{!!Form::label('name', 'Name')!!}
+		{!!Form::Text('name', null, ['class'=>'form-control'])!!}
+	</div>
+	<div class="form-group">
+		{!!Form::label('email', 'Email')!!}
+		{!!Form::email('email', null, ['class'=>'form-control'])!!}
+	</div>
+	<div class="form-group">
+		{!!Form::label('role_id', 'Role')!!}
+		{!!Form::select('role_id',[''=>'Select the role'] + $roles, null, ['class'=>'form-control'])!!}
+	</div>
+	<div class="form-group">
+		{!!Form::label('status', 'Status')!!}
+		{!!Form::select('is_active', array(1 =>'active', 0 =>'Inactive'), 0, ['class'=>'form-control'])!!}
+	</div>
+	<div class="form-group">
+		{!!Form::label('password', 'Password')!!}
+		{!!Form::password('password', ['class'=>'form-control'])!!}
+	</div>
+
+	<div class="form-group">
+		{!!Form::submit('Create user', ['class'=>'btn btn-primary'])!!}
+	</div>
+
+{!!Form::close()!!}
+
+
+
 @endsection
